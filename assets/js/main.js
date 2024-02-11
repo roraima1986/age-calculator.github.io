5a597184d8c13a71f062857bec2945c9438e5508
roraima1986 <96786551+roraima1986@users.noreply.github.com>
2024-02-11 14:28:36 -0300

    JS

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    // Declarar variable del boton y darle una accion
+    const btnCalculate = document.querySelector('.form__btn')   
+    
+    btnCalculate.addEventListener('click', () => {
+
+        
+        
+        
+    });
+})
